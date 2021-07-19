e0533964aca72a3f584b75cb018f05eb8fd758af
beapc_18 <beatrix.pc@gmail.com>
2021-07-19 15:58:06 +0200

    #19 create python package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     description='Bonn Activity Maps (BAM) API',
     url='https://github.com/bonn-activity-maps/aikapi',
-    packages=['aikapi'],
+    packages=['aikapi', 'aikapi.utils'],
     install_requires=['numpy==1.19.2',
                       'opencv-python==4.4.0.44',
                       'numba==0.51.2',
