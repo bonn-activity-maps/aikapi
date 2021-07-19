ab9870ae04adeab2c19c6d11ef769c6200ebe4a9
beapc_18 <beatrix.pc@gmail.com>
2021-07-19 15:40:58 +0200

    #19 add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='aikapi',
+    version='1.0.0',
+    description='Bonn Activity Maps (BAM) API',
+    url='https://github.com/bonn-activity-maps/aikapi',
+    packages=['aikapi'],
+    install_requires=['numpy==1.19.2',
+                      'opencv-python==4.4.0.44',
+                      'numba==0.51.2',
+                      'scipy==1.5.4'
+                      ],
+)
